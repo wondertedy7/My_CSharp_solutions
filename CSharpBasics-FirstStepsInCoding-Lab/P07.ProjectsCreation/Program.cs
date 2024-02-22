f788006c0c239a5d8dd016b1506f84de24d92837
wondertedy7 <wondertedy7@yahoo.com>
2024-02-22 12:30:18 +0200

    wondertedy7 added new files

---
+++ CSharpBasics-FirstStepsInCoding-Lab/P07.ProjectsCreation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P07.ProjectsCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int countProject = int.Parse(Console.ReadLine());
+            int timeNeeded = 3;
+            int hours = countProject * timeNeeded;
+
+            Console.WriteLine($"The architect {name} will need {hours} hours to complete {countProject} project/s.");
+
+
+
+        }
+    }
+}
